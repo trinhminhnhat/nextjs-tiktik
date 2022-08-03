eee6f2645eaa280a147894790e3d4a031f65a98d
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-08-03 20:39:53 +0700

    - update readme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
 import Navbar from '../components/Navbar';
@@ -16,19 +17,25 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     if (isSSR) return null;
 
     return (
-        <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
-            <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
-                <Navbar />
-                <div className="flex gap-6 md:gap-20 ">
-                    <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-                        <Sidebar />
-                    </div>
-                    <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-                        <Component {...pageProps} />
+        <>
+            <Head>
+                <title>TikTik</title>
+                <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+            </Head>
+            <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+                <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+                    <Navbar />
+                    <div className="flex gap-6 md:gap-20 ">
+                        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+                            <Sidebar />
+                        </div>
+                        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+                            <Component {...pageProps} />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </GoogleOAuthProvider>
+            </GoogleOAuthProvider>
+        </>
     );
 };
 
